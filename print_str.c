20ef68cf53662c74da75352284d0662b9d9034eb
Alexa <alexafdeveloper@gmail.com>
2019-03-18 21:25:34 +0000

    feature: _prints

--- print_str.c
+++ print_str.c
@@ -14,3 +14,30 @@ int _printc(va_list c)
 	_putchar(string);
 	return (0);
 }
+
+/**
+ *_prints - print a string
+ *@s: is a string
+ *Return: 0 it is ok, 1 it is fail
+ */
+
+int _prints(va_list s)
+{
+	unsigned int length = 0;
+	unsigned int i = 0;
+	char *string;
+
+	string = va_arg(s, char *);
+	if (string == NULL)
+	{
+	return (1);
+	}
+	length = _strlen(string);
+	while (i < length)
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	_putchar('\n');
+return (0);
+}
