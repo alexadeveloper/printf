30b4b4b59128facb27fa8f58711f764121a2a81b
Haroldo Vélez <hvelez020@gmail.com>
2019-03-18 22:38:06 +0000

    deleting print_num content

--- print_num.c
+++ print_num.c
@@ -1,15 +1,1 @@
 #include "holberton.h"
-
-/**
-  *_printd - Print a int
-  *@i: is a va_list that will receive a int
-  *Return: 0 it is ok, 1 it is fails
- */
-
-int _printd(va_list n)
-{
-	int number;
-	unsigned int length = 0;
-
-	number = va_arg(n, int);
-	
